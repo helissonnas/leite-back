889d363ef93b86343120ddb6d6d006d05a474212
helisson <helljho@gmail.com>
2020-01-22 17:34:57 -0300

    init server

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Test');
+const app = require('express')();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`🥛 listening on port ${port}!`));
